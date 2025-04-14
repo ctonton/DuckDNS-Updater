f07b0ba498e3e777885b096e0665ebe246aad21e
ctonton <51173996+ctonton@users.noreply.github.com>
2025-04-13 23:01:04 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ new6=$(dig -6 TXT +short o-o.myaddr.l.google.com @ns1.google.com 2>/dev/null | t
 [[ -z $new4 ]] && [[ -z $new6 ]] && exit 1
 [[ -z $new4 ]] && new4=$old4
 [[ -z $new6 ]] && new6=$old6
-[[ $new4 == $old4 ]] && [[ $new6 == $old6 ]] && [[ $(date +%s) -lt $(( $day0 + 86400 )) ]] && exit 0
+[[ $new4 == $old4 ]] && [[ $new6 == $old6 ]] && [[ $(date +%s) -lt $(( $day0 + 604800 )) ]] && exit 0
 curl -k "https://www.duckdns.org/update?domains=$domain&token=$token&ip=$new4&ipv6=$new6" &>/dev/null || exit 1
 sed -i "s/^old4.*/old4=$new4/;s/^old6.*/old6=$new6/;s/^day0.*/day0=$(date +%s)/" $0
 exit 0
@@ -28,7 +28,7 @@ EOF
       echo; read -p "Enter or paste your duckdns token here: " duckt
       sudo sed -i "s/your_domain_here/$duckd/;s/your_token_here/$duckt/" /root/.ddns.sh
       sudo chmod +x /root/.ddns.sh
-      sudo crontab -l 2>/dev/null | grep -q '.ddns.sh' || (sudo crontab -l 2>/dev/null; echo "*/15 * * * * /root/.ddns.sh &>/dev/null") | sudo crontab -
+      (sudo crontab -l 2>/dev/null | sed '/.ddns.sh/d'; echo "*/15 * * * * /root/.ddns.sh &>/dev/null") | sudo crontab -
       sudo service cron restart
       sudo bash /root/.ddns.sh
       break;;
