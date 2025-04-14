0bbdc0a560f586670eb3b1f1b653bdc4da64dfe4
ctonton <51173996+ctonton@users.noreply.github.com>
2025-04-13 22:34:47 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-echo; echo "Check for and install dependencies."
-curl -V &>/dev/null && dig -v &>/dev/null || (sudo apt update; sudo apt install -y curl host)
-echo; echo "Write the updater script>"
-sudo tee /root/.ddns.sh >/dev/null <<'EOF'
+clear; echo
+PS3="Select option: "
+select mode in Install Update Uninstall Quit; do
+  case $mode in
+    Install)
+      curl -V &>/dev/null && dig -v &>/dev/null || (sudo apt update; sudo apt install -y curl host)
+      sudo tee /root/.ddns.sh >/dev/null <<'EOF'
 #!/bin/bash
 ###
 domain=your_domain_here
@@ -21,13 +24,26 @@ curl -k "https://www.duckdns.org/update?domains=$domain&token=$token&ip=$new4&ip
 sed -i "s/^old4.*/old4=$new4/;s/^old6.*/old6=$new6/;s/^day0.*/day0=$(date +%s)/" $0
 exit 0
 EOF
-echo; read -p "Enter or paste your duckdns domain here: " duckd
-echo; read -p "Enter or paste your duckdns token here: " duckt
-sudo sed -i "s/your_domain_here/$duckd/;s/your_token_here/$duckt/" /root/.ddns.sh
-sudo chmod +x /root/.ddns.sh
-echo; echo "Add job to cron."
-sudo crontab -l 2>/dev/null | grep -q '.ddns.sh' || (sudo crontab -l 2>/dev/null; echo "*/15 * * * * /root/.ddns.sh &>/dev/null") | sudo crontab -
-sudo service cron restart
-echo; read -n 1 -s -r -p "DuckDNS updater script is installed. Press any key to update IP addresses and exit."
-sudo bash /root/.ddns.sh
+      echo; read -p "Enter or paste your duckdns domain here: " duckd
+      echo; read -p "Enter or paste your duckdns token here: " duckt
+      sudo sed -i "s/your_domain_here/$duckd/;s/your_token_here/$duckt/" /root/.ddns.sh
+      sudo chmod +x /root/.ddns.sh
+      sudo crontab -l 2>/dev/null | grep -q '.ddns.sh' || (sudo crontab -l 2>/dev/null; echo "*/15 * * * * /root/.ddns.sh &>/dev/null") | sudo crontab -
+      sudo service cron restart
+      sudo bash /root/.ddns.sh;;
+    Update)
+      echo; read -p "Enter or paste your duckdns domain here: " duckd
+      echo; read -p "Enter or paste your duckdns token here: " duckt
+      sudo sed -i "s/^domain=.*/domain=$duckd/;s/^token=.*/token=$duckt/" /root/.ddns.sh
+      sudo bash /root/.ddns.sh;;
+    Uninstall)
+      sudo crontab -l 2>/dev/null | sed '/.ddns.sh/d' | sudo crontab -
+      sudo service cron restart
+      sudo rm /root/.ddns.sh;;
+    Quit)
+      break;;
+    *)
+      echo "Incorect input.";;
+  esac
+done
 exit 0
